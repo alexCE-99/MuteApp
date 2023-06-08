e9ab2c88e256850942fa2eccbbe5648cb8db9bcb
alexCE-99 <alex.conroy55@gmail.com>
2023-06-08 19:19:46 +0100

    fixed crash if buttons are pressed while fields are empty

--- variables.py
+++ variables.py
@@ -7,14 +7,23 @@ running = False
 # functions to modify variables called from ui
 def modify_MuteKey(newMute):
     global muteKey
-    muteKey = newMute
+    if newMute == "":
+        muteKey = muteKey
+    else:
+        muteKey = newMute
 
 
 def modify_UnmuteKey(newUnmute):
     global unmuteKey
-    unmuteKey = newUnmute
+    if newUnmute == "":
+        unmuteKey = unmuteKey
+    else:
+        unmuteKey = newUnmute
 
 
 def modify_ExitKey(newExit):
     global exitKey
-    exitKey = newExit
+    if newExit == "":
+        exitKey = exitKey
+    else:
+        exitKey = newExit
